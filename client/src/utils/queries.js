2c591401884c31a8a722310811fe74ea607430e5
FROMERO63 <romerofrank63@gmail.com>
2023-11-27 17:09:06 -0800

    added in queries.js

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -3,19 +3,126 @@ import { gql } from '@apollo/client';
 export const QUERY_PROFILES = gql`
   query allProfiles {
     profiles {
-      _id
-      name
-      skills
+      userId
+      user
+      email
+      password
+      Already_Read {
+        bookId
+        title
+        author
+        ISBN
+        pub_Date
+        description
+        page_Count
+        img_Link
+        link
+        notes {
+          noteId
+          createdAt
+          userId
+          noteText
+          rating
+        }
+      }
+      To_Reads {
+        bookId
+        title
+        author
+        ISBN
+        pub_Date
+        description
+        page_Count
+        img_Link
+        link
+        notes {
+          noteId
+          createdAt
+          userId
+          noteText
+          rating
+      }
+      Wishlist {
+        bookId
+        title
+        author
+        ISBN
+        pub_Date
+        description
+        page_Count
+        img_Link
+        link
+        notes {
+          noteId
+          createdAt
+          userId
+          noteText
+          rating
+      }
     }
   }
 `;
 
-export const QUERY_SINGLE_PROFILE = gql`
-  query singleProfile($profileId: ID!) {
-    profile(profileId: $profileId) {
-      _id
-      name
-      skills
+export const QUERY_SINGLE_PROFILES = gql`
+  query getSingleProfile($userId: ID!) {
+    thought(userId: $userId) {
+    profiles {
+      userId
+      user
+      email
+      password
+      Already_Read {
+        bookId
+        title
+        author
+        ISBN
+        pub_Date
+        description
+        page_Count
+        img_Link
+        link
+        notes {
+          noteId
+          createdAt
+          userId
+          noteText
+          rating
+        }
+      }
+      To_Reads {
+        bookId
+        title
+        author
+        ISBN
+        pub_Date
+        description
+        page_Count
+        img_Link
+        link
+        notes {
+          noteId
+          createdAt
+          userId
+          noteText
+          rating
+      }
+      Wishlist {
+        bookId
+        title
+        author
+        ISBN
+        pub_Date
+        description
+        page_Count
+        img_Link
+        link
+        notes {
+          noteId
+          createdAt
+          userId
+          noteText
+          rating
+      }
     }
   }
 `;
