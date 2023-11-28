e9a26fe7fdffe167738d246f8c3212518344c70c
Giancarlo Whitaker <giancarlo.whitaker@gmail.com>
2023-11-27 19:12:20 -0800

    Update <BookDetails /> with props; update .gitignore

--- client/src/pages/BookDetails.jsx
+++ client/src/pages/BookDetails.jsx
@@ -1,7 +1,24 @@
-
+import { useParams } from 'react-router-dom';
+import { useQuery } from '@apollo/client';
 import Header from '../components/Header';
 
-function BookDetails() {
+import { QUERY_SINGLE_BOOK } from '../utils/queries';
+
+const BookDetails = () => {
+    // Use `useParams()` to retrieve value of the route parameter `:profileId`
+    const { bookId } = useParams();
+
+    const { loading, data } = useQuery(QUERY_SINGLE_BOOK, {
+      // pass URL parameter
+      variables: { bookId: bookId },
+    });
+  
+    const book = data?.book || {};
+  
+    if (loading) {
+      return <div>Loading...</div>;
+    }
+
     return (
     <div className="flex flex-row h-screen w-screen overflow-auto">    
         <Header />   
@@ -9,16 +26,16 @@ function BookDetails() {
             <div className="flex justify-between items-center flex-col w-5/6 h-5/6 overflow-auto bg-[#f8f2e5] rounded-lg">
                 <div className="flex flex-row justify-top items-top w-full h-3/5">
                     <div className="flex flex-col justify-top items-center w-1/4 p-6">
-                        <img className="rounded-sm" src='./img/avators/alice.jpg' alt="Alice in Wonderland" />
+                        <img className="rounded-sm" src={book.img_Link} />
                         <button className="text-[#001829] text-center bg-sky-500 w-28 h-10 m-1 shadow-inner shadow-white">Remove</button>
                     </div>
                     <div className="w-3/4 flex flex-col justify-top items-top">
-                        <p className="text-orange-950 text-left text-5xl justify-self-center font-serif antialiased mt-2">Alice in Wonderland <span className="text-blue-600 text-lg font-mono">(2019)</span></p>
-                        <p className="text-orange-950 text-left text-2xl justify-self-center font-serif antialiased my-2">by Carroll</p>
-                        <p className="text-orange-950 text-left text-lg justify-self-center font-serif antialiased">ISBN:</p>
-                        <p className="text-orange-950 text-left text-lg justify-self-center font-serif antialiased">Pages:</p>
-                        <p className="text-orange-950 text-left text-lg justify-self-center font-serif antialiased">Reviews: ?</p>
-                        <p className="text-orange-950 text-left text-lg justify-self-center h-56 font-serif antialiased overflow-y-auto">Alices Adventures in Wonderland is a classic childrens book by Lewis Carroll. The story is about a young girl named Alice who falls down a rabbit hole into a fantasy world. The world is populated by talking cards and animals who have tea parties and smoke pipes. </p>
+                        <p className="text-orange-950 text-left text-5xl justify-self-center font-serif antialiased mt-2">{book.title}<span className="text-blue-600 text-lg font-mono">({book.pub_Date})</span></p>
+                        <p className="text-orange-950 text-left text-2xl justify-self-center font-serif antialiased my-2">by {book.author}</p>
+                        <p className="text-orange-950 text-left text-lg justify-self-center font-serif antialiased">ISBN: {book.ISBN}</p>
+                        <p className="text-orange-950 text-left text-lg justify-self-center font-serif antialiased">Pages: {book.page_Count}</p>
+                        <p className="text-orange-950 text-left text-lg justify-self-center font-serif antialiased">Reviews: ???</p>
+                        <p className="text-orange-950 text-left text-lg justify-self-center h-56 font-serif antialiased overflow-y-auto">{book.description}</p>
                     </div>
                 </div>
                 <div className="flex flex-col justify-evenly bg-[#f8f2e5] items-center w-10/12 h-2/5 mb-6">
