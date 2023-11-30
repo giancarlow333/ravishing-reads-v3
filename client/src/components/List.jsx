47e3ea5c46f1d040c850d994801a5cc9f5332a47
Giancarlo Whitaker <giancarlo.whitaker@gmail.com>
2023-11-29 21:26:11 -0800

    Minor edits to <MyList />

--- client/src/components/List.jsx
+++ client/src/components/List.jsx
@@ -5,7 +5,7 @@ function List() {
 
     return (
         <div class="flex flex-row h-16 w-11/12 justify-between rounded-lg m-4 overflow-auto bg-sky-500  hover:bg-white outline outline-4 outline-white outline-offset-4 hover:outline-sky-600">
-            <p onClick={() => window.location.href = '/books'}class="w-full h-full font-extrabold text-black pt-3 px-4 text-3xl antialiased">Collection</p>
+            <p onClick={() => window.location.href = '/books'} class="w-full h-full font-extrabold text-black pt-3 px-4 text-3xl antialiased">Collection</p>
             <button class="w-full h-full font-extrabold text-black  px-4 text-3xl antialiased text-right">Remove</button>
         </div> 
     );
