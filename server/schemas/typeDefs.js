abd9e17e44d9bdaf6a05a6d35ecd00b6e1f30676
Giancarlo Whitaker <135566907+giancarlow333@users.noreply.github.com>
2023-11-29 20:04:32 -0800

    Update typeDefs.js

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,6 +1,6 @@
 const typeDefs = `
   type Profile {
-    _id: profileId
+    _id: ID!
     username: String
     email: String
     password: String
@@ -10,7 +10,7 @@ const typeDefs = `
   }
 
   type Note {
-    _id: noteId
+    _id: ID!
     noteText: String
     createdAt: String
     rating: Int
@@ -18,7 +18,7 @@ const typeDefs = `
 
   
   type Book {
-    _id: bookId
+    _id: ID!
     author: String
     ISBN: String
     pub_Date: String
@@ -34,24 +34,24 @@ const typeDefs = `
 
   type Query {
     profiles: [Profile]!
-    profile(_id: profileId!): Profile
+    profile(_id: ID!): Profile
     books:[Book]!
-    book(_id: bookId!): Book
+    book(_id: ID!): Book
     notes:[Note]!
-    getBookNote(_id: noteId!): Note
+    getBookNote(_id: ID!): Note
   }
 
 type Mutation {
     addProfile(usernamename: String!, email: String!, password: String!): Profile
     addBook(title: String!, author: String!, ISBN: String!, publisher: String!, pub_Date: String!, description: String!, page_Count: Int!, img_Link: String!, link: String!): Book
-    addToAlreadyRead(_id: bookId!, title:String!): Profile
-    addNote(_id: noteId!, noteText: String!, rating: Int!): Book
+    addToAlreadyRead(_id: ID!, title:String!): Profile
+    addNote(_id: ID!, noteText: String!, rating: Int!): Book
 
     # login(email: String!, password: String!): Auth
     
-    deleteProfile(_id: profileId!): Profile
-    removeBook(_id: bookId!): Book
-    removeNote(_id: bookId!): Book
+    deleteProfile(_id: ID!): Profile
+    removeBook(_id: ID!): Book
+    removeNote(_id: ID!): Book
   }
 `;
 
