af8118b84f41befade848b275cd133b9bcc5ca6b
Giancarlo Whitaker <giancarlo.whitaker@gmail.com>
2023-11-29 22:31:58 -0800

    Attempt to pass books to each list

--- client/src/components/List.jsx
+++ client/src/components/List.jsx
@@ -1,14 +1,25 @@
-//this compnent is meant to be used to display all the list user has created 
+//this component is meant to be used to display all the list user has created 
 import '../index.css';
+// Import `<Link>` component from React Router for internal hyperlinks
+import { Link } from 'react-router-dom';
+import { useQuery } from '@apollo/client';
+import { QUERY_BOOKS } from '../utils/queries';
 
 function List() {
+  const { loading, data } = useQuery(QUERY_BOOKS);
+  const books = data?.books || [];
 
-    return (
-        <div class="flex flex-row h-16 w-11/12 justify-between rounded-lg m-4 overflow-auto bg-sky-500  hover:bg-white outline outline-4 outline-white outline-offset-4 hover:outline-sky-600">
-            <p onClick={() => window.location.href = '/books'} class="w-full h-full font-extrabold text-black pt-3 px-4 text-3xl antialiased">Collection</p>
-            <button class="w-full h-full font-extrabold text-black  px-4 text-3xl antialiased text-right">Remove</button>
-        </div> 
-    );
+  return (
+    <div class="flex flex-row h-16 w-11/12 justify-between rounded-lg m-4 overflow-auto bg-sky-500  hover:bg-white outline outline-4 outline-white outline-offset-4 hover:outline-sky-600">
+      <Link
+        className="w-full h-full font-extrabold text-black pt-3 px-4 text-3xl antialiased"
+        to={`/books/`}
+        books={books}
+      >
+        Already Read
+      </Link>
+    </div> 
+  );
 }
 
 export default List;
