4b8ac2b5cdaca9cda4134c8e495fc7fa6ebbf62c
Giancarlo Whitaker <giancarlo.whitaker@gmail.com>
2023-11-30 21:33:26 -0800

    Fix token/auth issue in <SearchedBooks />

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,5 +1,5 @@
 import Header from '../components/Header';
-import SearchedBooks from '../components/SearchedBooks.jsx';
+import SearchedBook from '../components/SearchedBook.jsx';
 
 function Home() {
     return (
@@ -16,7 +16,7 @@ function Home() {
             </div>
 
             <div className="flex flex-col justify-top items-center flex-col w-full h-full overflow-auto">
-                <SearchedBooks />
+                <SearchedBook />
             </div>
         </div>
     </div>
