246a62528844f95d9aedfa15b1fc9c18ff5780cc
Giancarlo Whitaker <giancarlo.whitaker@gmail.com>
2024-04-06 19:23:55 -0700

    Make button visible only when list is selected

--- client/src/components/SearchedBooks.jsx
+++ client/src/components/SearchedBooks.jsx
@@ -23,6 +23,9 @@ function SearchedBook() {
     const [addToAlreadyRead, { data, loading, error }] = useMutation(ADD_TO_ALREADY_READ);
     // Example function to handle 'Create New List' option
     const handleSelectChange = (event) => {
+        // Make the button visible
+        document.getElementById("listButton").classList.remove("invisible");
+
         if (event.target.value === 'createNew') {
             // Trigger modal or input field to add new collection
             // For simplicity, let's assume a prompt is used
@@ -40,12 +43,6 @@ function SearchedBook() {
             console.log ("Already Read!"); 
         }
         else { console.log ("ELSE!"); }
-        let listSelectValue = event.target.value;
-        console.log("listSelectValue: ", listSelectValue);
-    };
-    const handleButtonClick = (event) => {
-        handleSelectChange(event);
-        console.log("Button clicked");
     };
     return ( // Currently uses placeholder data
         <div className="flex flex-row bg-white bg-opacity-90 w-10/12 h-3/5 shadow-md shadow-slate-900 rounded-md shrink ">
@@ -53,12 +50,13 @@ function SearchedBook() {
                 <img className="w-8/12 rounded-sm" src='/avators/alice.jpg' alt="Alice in Wonderland" />
               
                 <select id="listSelect" onChange={handleSelectChange} className=" text-sky-100 text-center text-lg bg-sky-600 w-4/6 h-1/6 m-1 shadow-inner shadow-white font-serif">
+                    <option value="default">Please select...</option>
                     {collections.map((collection) => (
                         <option key={collection.value} value={collection.value}>{collection.text}</option>
                     ))}
                     <option value="createNew">Create New List</option>
                 </select>
-                <button id="listButton" onClick={handleButtonClick} className=" text-sky-100 text-center text-lg bg-sky-600 w-4/6 h-1/6 m-1 shadow-inner shadow-white font-serif">This is a button</button>
+                <button id="listButton" className="invisible text-sky-100 text-center text-lg bg-sky-600 w-4/6 h-1/6 m-1 shadow-inner shadow-white font-serif">This is a button</button>
             </div>
             <div className="flex flex-col w-9/12 h-11/12 mr-8">
                 <p className="text-[#001828] text-left text-2xl justify-self-center font-bold antialiased mt-2">Alice in Wonderland <span className="text-blue-600 text-sm">(2019)</span></p>
