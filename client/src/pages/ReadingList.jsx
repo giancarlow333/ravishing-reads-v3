46c9e63ebecc817d88669ce7b1a3c53747e54b72
Giancarlo Whitaker <giancarlo.whitaker@gmail.com>
2024-05-17 15:52:03 -0700

    Add <ReadingList> and <AlreadyRead>

---
+++ client/src/pages/ReadingList.jsx
@@ -0,0 +1,25 @@
+import Header from '../components/Header';
+// Import `<Link>` component from React Router for internal hyperlinks
+import { useQuery } from '@apollo/client';
+import BookList from '../components/BookList';
+import { QUERY_TOREADS } from '../utils/queries';
+
+const Wishlist = () => {
+    const { loading, data } = useQuery(QUERY_TOREADS);
+    const books = data?.books || [];
+    console.log(books);
+    
+    if (!books.length) {
+        return <h3>No books Yet</h3>;
+    }
+
+    return (
+        <div className="flex flex-row h-screen w-screen overflow-auto">    
+            <Header />   
+                <div className="flex flex-col justify-top items-center flex-col w-full h-full overflow-auto bg-repeat"  style={{ backgroundImage: `url("./img/starBackground.png")` }}>
+                    <BookList books={books} />
+                </div>
+        </div>
+    ); 
+}
+export default Wishlist;
